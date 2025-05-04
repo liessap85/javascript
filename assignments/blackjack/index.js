6f69b0924d645ab29782e3fdd2cc2c7eeeaaa575
liessap85 <awslogcabin@outlook.com>
2025-05-04 16:15:50 +0100

    blackjack final

--- assignments/blackjack/index.js
+++ assignments/blackjack/index.js
@@ -1,12 +1,9 @@
-let firstCard = 10
-let secondCard = 4
-let sum = firstCard + secondCard
-// Added in an array to hold all the card numbers drawn
-const cards = [firstCard, secondCard]
-let newDraw = 0
+
+// Added in an array to hold all the card numbers drawn - as I want this to be amendable, I don't want this to be a const
+let cards = []
 
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 
 let message = ""
 let messageEl = document.getElementById("message-el")
@@ -14,36 +11,84 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+// JS object
+// Idelaly this would be held in session state and reloaded whenever user logs in again, 
+// but this does not yet have loging functionality, so is hard coded for now
+let player = {
+    name : "Hannah",
+    chips : 145 
+}
+
+let playerEl = document.getElementById("player-el")
+playerEl.innerText = `${player.name} : $${player.chips}`
+
 function startGame() {
+    cards = []    
+    sum = 0
+    // Should not be alive until game is started
+    isAlive = true
+    hasBlackJack = false
+    console.log(isAlive, hasBlackJack)
+    // Generate two random numbes
+    // Re-assign the cards and sum variables so that the game can start
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    sum = firstCard + secondCard
+
+    cards.push(firstCard, secondCard)
     renderGame()
 }
 
 function renderGame() {
-    sumEl.textContent = "Sum: " + sum
+    console.log(`rendergame = ${isAlive}, ${hasBlackJack}`)
+    //while (!hasBlackJack && isAlive) {
+        sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
         message = "You've got Blackjack!"
         hasBlackJack = true
+        console.log(`rendergame = ${isAlive}, ${hasBlackJack}`)
     } else {
         message = "You're out of the game!"
         isAlive = false
+        console.log(`rendergame = ${isAlive}, ${hasBlackJack}`)
     }
     messageEl.textContent = message
     // // Display just first two cards
     // cardsEl.textContent = `Cards: ${firstCard}, ${secondCard}`
-    // // Using the array
-    cardsEl.textContent = `Cards: ${cards}`
+
+    // // // Using the array (this one is the easiest to display but has commas)
+    // cardsEl.textContent = `Cards: ${cards}`
+
+    // Using an array and for loop
+    // Need to redefine the start each time, or array will duplicate old cards each time
+    cardsEl.textContent = "Cards: "
+
+    for (let i = 0; i<cards.length; i++) {
+        cardsEl.textContent += `${cards[i]} `
+    }
+   // }
+    
 }
 
 function newCard() {
+    if (isAlive) {
+        let newDraw = getRandomCard()
+        cards.push(newDraw)
+        sum += newDraw
+        renderGame()
+    }
+    else {
+        messageEl.textContent = "Please start a new game using the START GAME button."
+    }
 
-    newDraw = 5
-    cards.push(newDraw)
-    sum += newDraw
-    renderGame()
 }
 
+function getRandomCard() {
+    let cardValue = Math.floor(Math.random() * 10) + 2
+    return cardValue
+}
 
 
 
--- assignments/variables.js
+++ assignments/variables.js
@@ -1,82 +1,120 @@
-// Assignment ONE
+// // Assignment ONE
 
-// Create two variables, firstName and lastName
-let firstName = "Hannah"
+// // Create two variables, firstName and lastName
+// let firstName = "Hannah"
 
-let lastName = "Cheetham"
+// let lastName = "Cheetham"
 
-// Concatenate the two variables into a third variable called fullName
-// Use backticks to indicate string formatting
-let fullName = `${firstName} ${lastName}`  
-
-
-// Log fullName to the console
-console.log(fullName)
-
-//------------------------------------------------------------------------------------------------
-
-// Assignment TWO
-
-let name = "Linda"
-let greeting = "Hi there"
-
-// Create a function that logs out "Hi there, Linda!" when called
-
-function welcome(a, b) {
-    msg = `${b}, ${a}!`
-    console.log(msg)
-    // // Could also have the function return a value
-    // return msg
-}
-
-// Functions just need to be called
-welcome(name, greeting)
-
-// // Could also have the function return a value
-// console.log(welcome(name, greeting))
+// // Concatenate the two variables into a third variable called fullName
+// // Use backticks to indicate string formatting
+// let fullName = `${firstName} ${lastName}`  
 
 
-// Functions can be sync or async, if async we need to use something like a Promise.
+// // Log fullName to the console
+// console.log(fullName)
 
-//------------------------------------------------------------------------------------------------
+// //------------------------------------------------------------------------------------------------
 
-// Assignment THREE
+// // Assignment TWO
 
-let myPoints = 3
+// let name = "Linda"
+// let greeting = "Hi there"
 
-// Create two functions, add3Points() and remove1Point(), and have them
-// add/remove points to/from the myPoints variable
+// // Create a function that logs out "Hi there, Linda!" when called
 
-function add3Points() {
-    myPoints += 3
-}
+// function welcome(a, b) {
+//     msg = `${b}, ${a}!`
+//     console.log(msg)
+//     // // Could also have the function return a value
+//     // return msg
+// }
 
-function remove1Point() {
-    myPoints -= 1
-}
+// // Functions just need to be called
+// welcome(name, greeting)
 
-add3Points()
-add3Points()
-remove1Point()
-remove1Point()
-add3Points()
-remove1Point()
-add3Points()
-remove1Point()
-remove1Point()
+// // // Could also have the function return a value
+// // console.log(welcome(name, greeting))
 
 
-// Call the functions to that the line below logs out 10
-console.log(myPoints)
+// // Functions can be sync or async, if async we need to use something like a Promise.
 
-//------------------------------------------------------------------------------------------------
+// //------------------------------------------------------------------------------------------------
 
-// Assignment FOUR
+// // Assignment THREE
 
-// Try to predict what each of the lines will log out
-console.log("2" + 2) // "22"
-console.log(11 + 7) // 18
-console.log(6 + "5") // "65"
-console.log("My points: " + 5 + 9) // "My points: 59"  // A string will always beat an int (even when two ints are added together)
-console.log(2 + 2) // 4
-console.log("11" + "14") // "1114"
+// let myPoints = 3
+
+// // Create two functions, add3Points() and remove1Point(), and have them
+// // add/remove points to/from the myPoints variable
+
+// function add3Points() {
+//     myPoints += 3
+// }
+
+// function remove1Point() {
+//     myPoints -= 1
+// }
+
+// add3Points()
+// add3Points()
+// remove1Point()
+// remove1Point()
+// add3Points()
+// remove1Point()
+// add3Points()
+// remove1Point()
+// remove1Point()
+
+
+// // Call the functions to that the line below logs out 10
+// console.log(myPoints)
+
+// //------------------------------------------------------------------------------------------------
+
+// // Assignment FOUR
+
+// // Try to predict what each of the lines will log out
+// console.log("2" + 2) // "22"
+// console.log(11 + 7) // 18
+// console.log(6 + "5") // "65"
+// console.log("My points: " + 5 + 9) // "My points: 59"  // A string will always beat an int (even when two ints are added together)
+// console.log(2 + 2) // 4
+// console.log("11" + "14") // "1114"
+
+
+// //------------------------------------------------------------------------------------------------
+
+// let messages = [
+//     "Hey, how's it going?",        
+//     "I'm great, thank you! How about you?",
+//     "All good. Been working on my portfolio lately."
+// ]
+
+// let newMessage = "Same here!"
+// messages.push(newMessage)
+// console.log(messages)
+
+// // pop does not need any arguments, will always just do the last item
+// messages.pop()
+// console.log(messages)
+
+// //------------------------------------------------------------------------------------------------
+
+
+const badNums = [11, 12, 13]
+
+for (i=0; i<25;i++) {
+    let cardPull = Math.floor( Math.random()*13 ) + 1
+    if (cardPull == 1) {
+        cardPull = 11
+    }
+    // If card is in array
+    else if (badNums.includes(cardPull)) {
+        console.log("badNum" + cardPull)
+        cardPull = 10
+    }
+    console.log(cardPull)
+ }
+
+
+
